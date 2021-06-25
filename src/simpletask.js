0cf257e0a6e276c02fa1f2b786efdae701ed6f1f
Jacob Luallen <jluallen@fcc-jc.org>
2021-06-25 19:10:45 -0400

    work of factory function new tasks and maybe some functionality

--- src/simpletask.js
+++ src/simpletask.js
@@ -2,3 +2,7 @@
 Plan to hold the object that will serve as the simple tasks on the primary todo module and the tasks under the different projects. 
 */
 
+Const task = (title, description, dueDate, priority, notes, completed) => {
+    const getTitle = () => 
+
+}
