dee1fe719b8aaa40a6956c25b2a9e93c2c422958
Jacob Luallen <jluallen@fcc-jc.org>
2021-07-13 15:14:56 -0400

    Added Local Storage

--- src/data.js
+++ src/data.js
@@ -1,6 +1,21 @@
 
-    let taskArray = []
-    let projArray = []
-    let projTaskArray = []
+    let localStorageTaskArray = localStorage.getItem("taskArrayKey");
+    let localStorageProjArray = localStorage.getItem("projArrayKey");
+    let localStorageProjTaskArray = localStorage.getItem("projTaskArrayKey");
+    let taskArray = JSON.parse(localStorageTaskArray);
+    let projArray = JSON.parse(localStorageProjArray);
+    let projTaskArray = JSON.parse(localStorageProjTaskArray);
+    
 
+    /*
+let taskArray = JSON.parse(localStorageTaskArray);
+    let projArray = JSON.parse(localStorageProjArray);
+    let projTaskArray = JSON.parse(localStorageProjTaskArray);
+
+
+    let taskArray = [];
+    let projArray = [];
+    let projTaskArray = [];
+
+    */
 export {taskArray, projArray, projTaskArray}
--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ import * as projectDomFunctions from "./projectdom";
 //const firstTask = taskFactory("Something", "A thing I need to do", "Now", "very", "some stuff I need to do for this task", "no")
 //const secondTask = taskFactory("something secon", "A second thing to do", "now second", "very second", "some second stuff to do", "no second")
 //data.taskArray.push(firstTask);
+projectDomFunctions.makeProjList("projectContainer");
 taskListDOMFuncstions.makeTaskList("taskListContainer");
 taskListDOMFuncstions.makeAddTaskButton("taskListContainer");
 projectDomFunctions.makeAddProjButton("projectContainer");
 //transfer.pushData(firstTask);
 //transfer.pushData(secondTask);
 //taskStorage().taskArray[0].returnInput();
-
+//localStorage.clear();
 //console.log(data.taskArray[0]);
 //console.log(data.taskArray[1]);
 
